7f499b28761e3a5e1c81c38aea57971fc8728fca
Shreena-Chris <120237349+Shreena-Chris@users.noreply.github.com>
2023-10-05 10:27:43 +0530

    Add files via upload

---
+++ day 11/src/main/java/com/astrasquad/shreenachristop/ShreenachristopApplication.java
@@ -0,0 +1,13 @@
+package com.astrasquad.shreenachristop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShreenachristopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShreenachristopApplication.class, args);
+	}
+
+}
