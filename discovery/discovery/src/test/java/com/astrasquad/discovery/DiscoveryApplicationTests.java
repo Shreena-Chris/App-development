b6bc08428a7c40caddc3170730ee53e07370c163
Shreena-Chris <120237349+Shreena-Chris@users.noreply.github.com>
2023-10-16 16:11:21 +0530

    Add files via upload

---
+++ discovery/discovery/src/test/java/com/astrasquad/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astrasquad.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
